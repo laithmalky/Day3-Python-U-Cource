3151999a7cddb2530864e3fb7cca9e424734c43b
Laith Malky <laith.malky@gmail.com>
2024-12-23 20:36:49 +0200

    commit

--- day3.py
+++ day3.py
@@ -6,9 +6,11 @@ if height >= 120 :
     print("You can play the game :)")
     age = int(input("Whats your age? "))
     if age >= 18:
-        print("You have to pay $12 U.S.D. for a ticket")
+        print("You have to pay $12 U.S.D. for a ticket!")
+    elif age <= 12:
+        print("You have to pay $5 U.S.D. for the ticket!")
     else:
-        print("You have to pay $7 U.S.D. for a ticket")
+        print("You have to pay $7 U.S.D. for a ticket!")
 else :
     print("For sadly you cant play the Game :(") 
 
